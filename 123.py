3fbf3fbae3eb27ea8eca106fbebf1912ac86874c
Auony <49891653+Auony@users.noreply.github.com>
2019-04-23 17:20:25 +0800

    Delete 123.py

--- 123.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from time import sleep
-import os
-#######################################
-driver = webdriver.Chrome()
-driver,maximize_window()
-driver.implicitly_wait(26)
-url = "https://www.baidu.com/"
-driver.get(url)
